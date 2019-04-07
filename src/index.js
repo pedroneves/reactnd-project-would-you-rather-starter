71af601d51cc3f732dc12349417fa9cb3d284ada
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-07 13:22:00 -0300

    feat: Adding basic structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
+import reducers from './reducers'
+import middlewares from './middlewares'
+
+const store = createStore(reducers, middlewares);
+
+const app = (
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>
+)
+
+const root = document.getElementById('root');
+
+ReactDOM.render(app, root);
