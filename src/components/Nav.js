31450121b8821d4b5d8d0825c9d1966ca9484dcc
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-08 23:25:43 -0300

    feat: adding structure to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,22 @@ import { Row, Col } from 'antd';
 
 class Nav extends Component {
 	render() {
+		const style = { textAlign: 'center' }
+
 		return (
-			<Row type="flex" gutter={20} justify="center">
-				<Col>Unanswered</Col>
-				<Col>Answered</Col>
-				<Col>New</Col>
-				<Col>Leaderboard</Col>
+			<Row style={{marginTop: '20px'}} type="flex" justify="space-around">
+				<Col style={style} >
+					<p>Unanswered</p>
+				</Col>
+				<Col style={style} >
+					<p>Answered</p>
+				</Col>
+				<Col style={style} >
+					<p>New</p>
+				</Col>
+				<Col style={style} >
+					<p>Leaderboard</p>
+				</Col>
 			</Row>
 		)
 	}
