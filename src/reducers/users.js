5375fb761587d0cf2c3bbdebdcd5e50593b2c359
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-09 21:56:45 -0300

    feat: Adding users reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,27 @@
-export default function(state=[], action) {
+import Actions from '../actions';
+
+export default function(state={}, action) {
 	switch(action.type) {
+		case Actions.Users.GET_USERS_START:
+			return {
+				...state,
+				isLoading: true
+			}
+		case Actions.Users.GET_USERS_SUCCESS:
+			const { users } = action;
+
+			return {
+				...state,
+				isLoading: false,
+				byId: { ...users },
+				ids: Object.keys(users)
+			}
+		case Actions.Users.GET_USERS_FAILED:
+			return {
+				...state,
+				isLoading: false,
+				error: action.error
+			}
 		default:
 			return state;
 	}
