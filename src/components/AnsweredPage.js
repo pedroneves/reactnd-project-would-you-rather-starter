54dd66d83502fa87bd9aab2993c6e064f054c1ac
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-15 08:36:57 -0300

    feat: Adding basic pages

---
+++ src/components/AnsweredPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AnsweredPage extends Component {
+	render () {
+		return (
+			<h1>Answred page</h1>
+		)
+	}
+}
+
+export default AnsweredPage;
---
+++ src/components/UnansweredPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UnansweredPage extends Component {
+	render () {
+		return (
+			<h1>Unanswred page</h1>
+		)
+	}
+}
+
+export default UnansweredPage;
