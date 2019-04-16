99509bee1e7937e95ad7d0d0d38f7d312ec8a683
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-16 13:17:03 -0300

    feat: Connecting QuestionPage with store

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,3 +1,4 @@
+import Moment from 'moment';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Avatar, Card, Col, Row, Tooltip } from 'antd';
@@ -7,21 +8,22 @@ import '../styles/question-page.css'
 class QuestionCard extends Component {
 	render () {
 		const { question={}, author={}, vote } = this.props;
-		const { authorName='authorName', avatarURL='avatarURL' } = author;
-		const { optionOne='unkown_option1', optionTwo='unkown_option2' } = question;
+		const { name='authorName', avatarURL='avatarURL' } = author;
+		const { optionOne={}, optionTwo={} } = question;
 
-		const votedOne = vote === 'one';
-		const votedTwo = vote === 'two';
+		const votedOne = vote === 'optionOne';
+		const votedTwo = vote === 'optionTwo';
+		const hasVoted = votedOne || votedTwo;
 
 		return (
 			<div className="question-page">
 				<Row type="flex" justify="center">
 					<h1 className="title">Would you rather...</h1>
 				</Row>
-				<Row type="flex" justify="space-around">
+				<Row className="option-container" type="flex" justify="space-around">
 					<Col md={6} sm={8} xs={24}>
 						<Tooltip title="You voted this" visible={votedOne}>
-							<p className="option">{optionOne}</p>
+							<p className="option">{optionOne.text}</p>
 						</Tooltip>
 					</Col>
 					<Col md={6} sm={8} xs={24}>
@@ -29,19 +31,24 @@ class QuestionCard extends Component {
 					</Col>
 					<Col md={6} sm={8} xs={24}>
 						<Tooltip title="You voted this" visible={votedTwo}>
-							<p className="option">{optionTwo}</p>
+							<p className="option">{optionTwo.text}</p>
 						</Tooltip>
 					</Col>
 				</Row>
-				<Row type="flex" justify="center">
+				<Row className="author-container" type="flex" justify="center">
 					<Col xl={8} lg={12} md={14} sm={18} xs={23}>
 						<Card>
-							<h3>Posted By</h3>
+							<h4>Posted By</h4>
 
 							<Row type="flex" justify="start" align="middle" gutter={20}>
-								<Col> <Avatar size="large" icon="user" src={avatarURL} /> </Col>
-								<Col> <h3>{authorName}</h3> </Col>
-								<Col>  </Col>
+								<Col>
+									<Avatar size={55} icon="user" src={avatarURL} />
+								</Col>
+								<Col>
+									<h3>{name}</h3>
+									<h4 disabled={true}>Asked at {Moment(question.timestamp).format('MM-DD-YYYY HH:mm')}</h4>
+								</Col>
+								<Col></Col>
 							</Row>
 						</Card>
 					</Col>
@@ -54,7 +61,7 @@ class QuestionCard extends Component {
 QuestionCard.propTypes = {
 	author: PropTypes.object.isRequired,
 	question: PropTypes.object.isRequired,
-	vote: PropTypes.oneOf(['one', 'two']),
+	vote: PropTypes.oneOf(['optionOne', 'optionTwo']),
 }
 
 export default QuestionCard;
--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,13 +1,27 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import QuestionCard from './QuestionCard';
 
 class QuestionPage extends Component {
 	render () {
+		const questionId = this.props.match.params.id;
+
+		const question = Object.assign({}, this.props.questions.byId[questionId]);
+		const author = Object.assign({}, this.props.users.byId[question.author]);
+
+		let vote = this.props.authedUser.answers[questionId];
+
 		return (
-			<QuestionCard />
+			<QuestionCard question={question} author={author} vote={vote} />
 		)
 	}
 }
 
-export default QuestionPage;
+function mapStateToProps (state) {
+	const { authedUser, questions, users } = state;
+	return { authedUser, questions, users };
+}
+
+export default withRouter(connect(mapStateToProps)(QuestionPage));
