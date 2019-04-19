377cc1c0722703143d59b2c690ab7830bd23c84b
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-19 13:04:11 -0300

    refactor: Adding complementary text

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -1,16 +1,29 @@
-import { Card } from 'antd'
+import { Card, Typography } from 'antd'
 import React, { Component } from 'react';
 
 import PollItem from './PollItem';
 
+const { Text } = Typography;
+
 class PollList extends Component {
 	render () {
 		const { questions=[] } = this.props;
+
+		if (questions.length === 0) {
+			return (
+				<Card>
+					<Text disabled>No questions available...</Text>
+				</Card>
+			)
+		}
+
 		return (
 			<Card>
-				{questions.map(question => {
-					return <PollItem key={question.id} question={question} />
-				})}
+				{
+					questions.map(question => {
+						return <PollItem key={question.id} question={question} />
+					})
+				}
 			</Card>
 		)
 	}
